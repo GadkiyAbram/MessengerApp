a7b391ab63ba0c55644ec9b36f2965dcca9436c6
GadkiyAbram <alex-abram@bk.ru>
2020-10-23 10:38:32 +0500

    updated token key

--- app/src/main/java/com/example/messengerapp/Fragments/APIService.java
+++ app/src/main/java/com/example/messengerapp/Fragments/APIService.java
@@ -11,7 +11,7 @@ import retrofit2.http.POST;
 public interface APIService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAAIwdKjMM:APA91bFKdnUzW58IMn_fWqzh12-dLP6vvD-ssO_tb9jyXKusvm3T8uLUF4jHL9waSHwGo4IhZJb1I6uDBQMLmNj_o1SFW6I_4TyCsM1FLf6i4EG9-SOCaU1wKF6Vc-yfT_i8geldbkE"
+            "Authorization:key=AAAAIwdKjMM:APA91bFKdnUzW58IMn_fWqzh12-dLP6vvD-ssO_tb9jyXKusvm3T8uLUF4jHL9waSHwGo4IhZJb1I6uDBQMLmNj_o1SFW6I_4TyCsM1FLf6i4EG9-SOCaU1wKF6Vc-yfT_i8geldbkE_"
     })
 
     @POST("fcm/send")
